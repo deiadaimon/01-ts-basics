d16fa33791dd62eb792e8605c6291c56cf492576
Deia Daimon <deiadaimon@gmail.com>
2025-07-13 12:04:17 +0200

    Update task-2.ts

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,15 +1,13 @@
-const product: Product = {
-  id: 1,
-  title: "Tablet",
-  description: "Compact and fast",
-};
-
-// *чи важливий порядок - спершу інтерфейс, а потім змінна?
-
 interface Product {
     readonly id: number;
     title: string;
     description?: string;
 }
+
+const product: Product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
 
 console.log(`Product: ${JSON.stringify(product)}`);
